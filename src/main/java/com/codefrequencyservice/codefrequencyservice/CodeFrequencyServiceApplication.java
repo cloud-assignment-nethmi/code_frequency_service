c4c28402c3c033e1179d6b02fc11fd2bb00c00b0
Nethmi97 <nnimesha97@gmail.com>
2023-12-08 21:26:57 +0530

    initial commit - code freq ms

---
+++ src/main/java/com/codefrequencyservice/codefrequencyservice/CodeFrequencyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codefrequencyservice.codefrequencyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFrequencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFrequencyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codefrequencyservice/codefrequencyservice/CodeFrequencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefrequencyservice.codefrequencyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFrequencyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
